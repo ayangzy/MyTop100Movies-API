a34f0a24b82629d6313e673ecdf795aa31b935ad
felix ayange <felixdecoder2020@gmail.com>
2023-05-16 15:15:09 +0100

    refactor auth controller to make it slimer by handling validation in the service class

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,31 +3,16 @@ const {
   createdResponse,
   successResponse,
 } = require('../responses/apiResponses')
-const CustomError = require('../errors')
-const { BadRequestError } = require('../errors')
 
 exports.register = async (req, res) => {
   const { name, email, password } = req.body
-  try {
-    const payload = await authService.registerUser(name, email, password)
-    createdResponse(res, 'User successfully SignedUp', payload)
-  } catch (error) {
-    if (error instanceof CustomError.BadRequestError) {
-      throw new BadRequestError(error.message)
-    }
-    throw error
-  }
+
+  const payload = await authService.registerUser(name, email, password)
+  createdResponse(res, 'User successfully SignedUp', payload)
 }
 
 exports.login = async (req, res) => {
   const { email, password } = req.body
-  try {
-    const payload = await authService.loginUser(email, password)
-    successResponse(res, 'User loggedIn successfully', payload)
-  } catch (error) {
-    if (error instanceof CustomError.BadRequestError) {
-      throw new BadRequestError(error.message)
-    }
-    throw error
-  }
+  const payload = await authService.loginUser(email, password)
+  successResponse(res, 'User loggedIn successfully', payload)
 }
--- services/authService.js
+++ services/authService.js
@@ -2,53 +2,61 @@ const User = require('../models/userModel')
 const CustomError = require('../errors')
 
 async function registerUser(name, email, password) {
-  const emailAlreadyExist = await User.findOne({ email })
+  try {
+    const emailAlreadyExist = await User.findOne({ email })
 
-  if (emailAlreadyExist) {
-    throw new CustomError.BadRequestError('User already exist')
+    if (emailAlreadyExist) {
+      throw new CustomError.BadRequestError('User already exist')
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    })
+
+    const tokenUser = user.createJWT()
+
+    const payload = {
+      accessToken: tokenUser,
+      user: {
+        email: email,
+        name: name,
+      },
+    }
+
+    return payload
+  } catch (error) {
+    throw error
   }
-
-  const user = await User.create({
-    name,
-    email,
-    password,
-  })
-
-  const tokenUser = user.createJWT()
-
-  const payload = {
-    accessToken: tokenUser,
-    user: {
-      email: email,
-      name: name,
-    },
-  }
-
-  return payload
 }
 
 async function loginUser(email, password) {
-  const user = await User.findOne({ email: email })
-  if (!user) {
-    throw new CustomError.BadRequestError('Invalid credentials')
-  }
+  try {
+    const user = await User.findOne({ email: email })
+    if (!user) {
+      throw new CustomError.BadRequestError('Invalid credentials')
+    }
 
-  const isPasswordCorrect = await user.comparePassword(password)
-  if (!isPasswordCorrect) {
-    throw new CustomError.BadRequestError('Invalid credentials')
-  }
+    const isPasswordCorrect = await user.comparePassword(password)
+    if (!isPasswordCorrect) {
+      throw new CustomError.BadRequestError('Invalid credentials')
+    }
 
-  const tokenUser = user.createJWT()
-  const userObjects = {
-    email: user.email,
-    name: user.name,
-  }
-  const payload = {
-    accessToken: tokenUser,
-    user: userObjects,
-  }
+    const tokenUser = user.createJWT()
+    const userObjects = {
+      email: user.email,
+      name: user.name,
+    }
+    const payload = {
+      accessToken: tokenUser,
+      user: userObjects,
+    }
 
-  return payload
+    return payload
+  } catch (error) {
+    throw error
+  }
 }
 
 module.exports = {
